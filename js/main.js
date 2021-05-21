4eb4cea5d40d8353cef5a2e84f4cb7f78d315e30
Mclee644 <644mcl@gmail.com>
2021-05-21 18:41:51 +0000

    may 21 updates

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ const nav=document.getElementById('nav');
 const navBar=document.getElementById('navBar');
 const toggle=document.getElementById('toggle');
 
-document.onclink =function(e){
-    if(e.target.id!='nav'&& e.target.id!='toggle'&&e.target.id!='navbar'){
-        toggle.classList.remove('active');
+
+ document.onclick = function (e){
+     if(e.target.id!=='nav'&&e.target.id!=='toggle'&&e.target.id!=='navBar'){
         navBar.classList.remove('active');
-    }
-}
+         toggle.classList.remove('active');
+     }{
+         console.log('fioh na anfa......, try again')
+     }
+ }
+
+
 toggle.onclick= function(){
     toggle.classList.toggle('active');
     navBar.classList.toggle('active');
